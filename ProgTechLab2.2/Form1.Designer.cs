f12f42fdcc8822ef16a0d316f8e36eb2d538f3e7
YuEgor <rezervdesu15@gmail.com>
2022-09-18 19:38:28 +0800

    menu design

--- ProgTechLab2.2/Form1.Designer.cs
+++ ProgTechLab2.2/Form1.Designer.cs
@@ -31,38 +31,50 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(364, 134);
+            this.label1.Location = new System.Drawing.Point(105, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(250, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Введите ваши числа через пробел";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(221, 124);
+            this.textBox1.Location = new System.Drawing.Point(12, 94);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.Size = new System.Drawing.Size(426, 27);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(311, 223);
+            this.button1.Location = new System.Drawing.Point(188, 141);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "узнать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(409, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сколько раз в последовательности чисел меняется знак?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(456, 195);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -78,5 +90,6 @@
         private Label label1;
         private TextBox textBox1;
         private Button button1;
+        private Label label2;
     }
 }
--- ProgTechLab2.2/Form1.cs
+++ ProgTechLab2.2/Form1.cs
@@ -6,5 +6,10 @@ namespace ProgTechLab2._2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
